cf03b53b8da03905cbc646cb97109aed244bca11
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-07-04 21:31:37 -0500

    feat: Implement WebSocket on the server

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
-const server = require('./src/app');
+const app = require('./src/app');
+const SocketIO = require('socket.io');
 
-server.listen(3001, () => {
+const server = app.listen(3001, () => {
     console.log('Server listening on port 3001');
 })
+
+const io = SocketIO(server);
+
+io.on('connection', (socket: any) => {
+    
+    socket.on('chat:typing', (username: string) => {
+        socket.broadcast.emit('chat:typing', username);
+    })
+
+    socket.on('chat:message', (data: any) => {
+        io.sockets.emit('chat:message', data);
+    });
+});
